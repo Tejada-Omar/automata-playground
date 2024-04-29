b2a351156fabed18f0bd9948941ebe2f7c27fee3
Omar Tejada <tejada.omar03@gmail.com>
2024-04-29 03:24:34 -0600

    feat(ui): describe basic button interface ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import Button from "./components/Button";
 import Editor from "./components/Editor";
 
 export default function Home() {
-  return <Editor />;
+  return (
+    <div className="block space-y-2 max-w-screen-md">
+      <div className="flex flex-wrap flex-auto items-center">
+        <Button text="Load Machine" />
+        <Button text="Revert to Diagram" />
+      </div>
+      <Editor />
+    </div>
+  );
 }
